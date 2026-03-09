693e987ce27ea7c66f27a1716d10aa660f16377e
Johnson <johnsonchong0123@gmail.com>
2026-03-09 14:13:47 +0800

    test: add Patrol framework for integration testing
    - Add patrol ^4.3.0 and integration_test dependencies
    - Configure patrol app name and package
    - Update generated plugin registrant for macOS

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import flutter_secure_storage_darwin
 import google_sign_in_ios
+import patrol
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  PatrolPlugin.register(with: registry.registrar(forPlugin: "PatrolPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
